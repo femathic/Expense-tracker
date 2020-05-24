355262d052b6bf8063df0df670f96a3e9d4e4db8
Femi Opanuga <femathic@gmail.com>
2020-05-24 17:14:18 +0100

    Finish up with the backend

---
+++ client/build/precache-manifest.00d277f685d407e792f578d341968e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0b79705715ef9fe79265f0eadd17a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cfab8a133d3371af28a",
+    "url": "/static/css/main.571e6405.chunk.css"
+  },
+  {
+    "revision": "6a8ccbd17323139d9c29",
+    "url": "/static/js/2.83135a7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83135a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cfab8a133d3371af28a",
+    "url": "/static/js/main.87f5769c.chunk.js"
+  },
+  {
+    "revision": "ded419595feb73f778e6",
+    "url": "/static/js/runtime-main.23acf746.js"
+  }
+]);
