70fdd8d766753426b3f816fe762054d6da3257e3
uncleteslim <teslimolukazeem@gmail.com>
2020-06-15 08:49:46 -0500

    rounded up react functions

--- client/build/precache-manifest.baa0675980384dd4c69feeadffec6700.js
+++ client/build/precache-manifest.baa0675980384dd4c69feeadffec6700.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa0b79705715ef9fe79265f0eadd17a4",
+    "revision": "9c77b35d82bc703351d04b0fe7174d69",
     "url": "/index.html"
   },
   {
-    "revision": "0cfab8a133d3371af28a",
+    "revision": "51385b21aae139e7f958",
     "url": "/static/css/main.571e6405.chunk.css"
   },
   {
-    "revision": "6a8ccbd17323139d9c29",
-    "url": "/static/js/2.83135a7f.chunk.js"
+    "revision": "30e430e739fad9e954e2",
+    "url": "/static/js/2.a03002b9.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.83135a7f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a03002b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0cfab8a133d3371af28a",
-    "url": "/static/js/main.87f5769c.chunk.js"
+    "revision": "51385b21aae139e7f958",
+    "url": "/static/js/main.d935bacc.chunk.js"
   },
   {
     "revision": "ded419595feb73f778e6",
